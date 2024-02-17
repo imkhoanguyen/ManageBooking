bc9937e38b67be9a631e1729d8207427060952bc
Khoa Nguyen <khoanguyen.coder@gmail.com>
2024-02-17 23:21:22 +0700

    Villa CRUD

--- ManageBooking.Web/Controllers/VillaController.cs
+++ ManageBooking.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using ManageBooking.Infrastructure.Data;
+﻿using ManageBooking.Domain.Entities;
+using ManageBooking.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ManageBooking.Web.Controllers
@@ -18,6 +19,58 @@ namespace ManageBooking.Web.Controllers
 
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj is null) return RedirectToAction("Error", "Home");
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj is null) return RedirectToAction("Error", "Home");
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(x => x.Id == obj.Id);
+            if(objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
