771f558e7f24021d5e7f0214e3305ca277ec3eb2
Khoa Nguyen <khoanguyen.coder@gmail.com>
2024-02-17 13:38:51 +0700

    Villa Model

---
+++ ManageBooking.Web/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using ManageBooking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageBooking.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
