5f849fdaae0cd8ada1d67d74fcee2126f3643a9d
Khoa Nguyen <khoanguyen.coder@gmail.com>
2024-02-29 21:44:43 +0700

    Home Page

--- ManageBooking.Web/Controllers/HomeController.cs
+++ ManageBooking.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using ManageBooking.Application.Common.Interfaces;
 using ManageBooking.Web.Models;
+using ManageBooking.Web.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +8,22 @@ namespace ManageBooking.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
+                Nights = 1,
+                CheckInDate = DateOnly.FromDateTime(DateTime.Now),
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
