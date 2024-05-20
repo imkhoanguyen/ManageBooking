e61072f800bc3f6dca6f8f04a134de8dd43b5df0
khoa <ankhoa.dev@gmail.com>
2024-05-20 12:11:09 +0700

    17 done

--- ManageBooking.Application/Common/Interfaces/IRepository.cs
+++ ManageBooking.Application/Common/Interfaces/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ManageBooking.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -10,8 +11,8 @@ namespace ManageBooking.Application.Common.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
-        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         bool Any(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Remove(T entity);
